de7a387dea84889bd2a7f4041d89b57bf7c8d453
dabana <david.banville2712@gmail.com>
2018-09-18 12:37:18 -0400

    compute norm and theta to produce u and v is more reliable.

--- scatter_slider.py
+++ scatter_slider.py
@@ -171,33 +171,25 @@ def update_graph(rx_pos_index, xaxis_type, yaxis_type, xaxis_col_val, yaxis_col_
 def update_quiver(Output_dict):
     Output_dict_dser = json.loads(Output_dict)
     dft = pd.read_json(Output_dict_dser[axis_list[0]], orient='split')['time_us']
-    dfx = pd.read_json(Output_dict_dser[axis_list[1]], orient='split')
-    dfy = pd.read_json(Output_dict_dser[axis_list[2]], orient='split')
+    dfx = pd.read_json(Output_dict_dser[axis_list[3]], orient='split')
+    dfy = pd.read_json(Output_dict_dser[axis_list[4]], orient='split')
 
     xx = np.array([float(pos) for pos in rx_pos])
-    yy = dft.values
+    yy = np.log(dft.values)
     x, y = np.meshgrid(xx, yy)
-    print(x)
-    print(str(len(x)))
-    print(y)
-    print(str(len(y)))
-    #ix, iy = np.meshgrid(np.arange(0, len(xx), 1), np.arange(0, len(yy), 1))
-    #x = xx.repeat(yy.size)
-    #y = yy.repeat(xx.size)
-    
-    u = dfx.values
-    u = np.sign(u) * np.log(np.abs(u))
-    v = dfy.values
-    v = np.sign(v) * np.log(np.abs(v))
+    scale = 1e3
+    u = dfx.values 
+    #u = u * scale
+    #u = np.sign(u) * np.log(np.abs(u))
+    v = dfy.values 
+    #v = v * scale
+    #v = np.sign(v) * np.log(np.abs(v))
 
-    #x,y = np.meshgrid(np.arange(0, 3, .2), np.arange(0, 2, .2))
-    #u = np.cos(x)*y
-    #v = np.sin(x)*y
-
-    print("x is size " + str(x.size))
-    print("y is size " + str(y.size))
-    print("u is size " + str(u.size))
-    print("v is size " + str(v.size))
+    norm = (u ** 2 + v ** 2) * 0.5
+    norm = np.log(norm)
+    angle = np.arctan(v/u)
+    u = norm * np.cos(angle)
+    v = norm * np.sin(angle)
 
     fig = ff.create_quiver(x, y, u, v,
                         scale=1,
